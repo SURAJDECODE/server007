53e428e788895ead545d395eb906623cd8fcc3e8
SURAJDECODE <78976730+SURAJDECODE@users.noreply.github.com>
2021-05-27 15:57:48 +0530

    subject1
    test

---
+++ new.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a=24;
+   float b =55.58f;
+   char  c= 'S';
+   double d=5.545465,x;
+
+   x = a+b+d;
+   cout<<x<<endl<<a<<endl<<b<<endl<<c<<endl<<d<<endl;
+   
+  return 0;
+
+}
